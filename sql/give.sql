d246cd6dcd1ee7f5045e21cffc3898760e2d109d
david bartz <the2baddavid@gmail.com>
2012-03-23 10:14:21 -0400

    Updates with the queries that i missed

--- sql/give.sql
+++ sql/give.sql
@@ -10,7 +10,7 @@ CREATE TABLE student_contact(
 	m_phone CHAR(9),
 	w_phone CHAR(9),
 	mail VARCHAR(40)) ENGINE INNODB;
-	
+
 CREATE TABLE pro_contact(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
 	title VARCHAR(30),
@@ -21,29 +21,29 @@ CREATE TABLE pro_contact(
 	m_phone CHAR(9),
 	w_phone CHAR(9),
 	mail VARCHAR(40) ENGINE INNODB;
-	
+
 CREATE TABLE contact_history(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
 	contact_id INT,
 	program_id INT) ENGINE INNODB;
-	
+
 CREATE TABLE addr(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	street VARCHAR(50),
 	city VARCHAR(30),
 	state VARCHAR(30),
 	zip CHAR(5)) ENGINE INNODB;
-	
+
 CREATE TABLE agency(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(20),
 	descript VARCHAR(1000),
-	p_contact_id INT UNSIGNED, 
+	p_contact_id INT UNSIGNED,
 	addr INT UNSIGNED,
 	mail VARCHAR(40),
 	phone CHAR(9),
 	fax CHAR(9)) ENGINE INNODB;
-	
+
 CREATE TABLE program(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	agency INT UNSIGNED NOT NULL,
@@ -54,12 +54,13 @@ CREATE TABLE program(
 	descript VARCHAR(400),
 	referal BOOL,
 	season BINARY(4),
-	times BINARY(24),	
-	notes VARCHAR(400),
-	issues BINARY(40),
-	duration VARCHAR(50)) ENGINE INNODB;
-	
+	times BINARY(24),
+	issue1 INT UNSIGNED,
+	issue2 INT UNSIGNED,
+	issue3 INT UNSIGNED,
+	duration INT UNSIGNED,
+	notes VARCHAR(400)) ENGINE INNODB;
+
 CREATE TABLE issues(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(20)) ENGINE INNODB;
-	
