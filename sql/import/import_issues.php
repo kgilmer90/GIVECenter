549be9682284df0abecba575b4f3e9538a3b22ba
david bartz <the2baddavid@gmail.com>
2012-04-19 20:38:59 -0400

    Added import and backup of issues

---
+++ sql/import/import_issues.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+mysql_connect('localhost', 'bgs', 'dki2012!');
+mysql_select_db('give_ctr_agencies');
+
+$string = "alumni,animals,children,disabilities,disasters,education,elderly,environment,female issues,fine arts,general service,health,male issues,minority issues,office,patriotic,poverty,PR,recreation,religious,service leaders,technology";
+$delim = ",";
+
+$issues = explode($delim, $string);
+
+foreach($issues as $temp){
+    $query = "INSERT INTO issues (name) VALUES ('$temp')";
+    mysql_query($query) or die(mysql_error());
+}
+
+?>
