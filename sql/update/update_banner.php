e1dd4cfe475ba60fbb8f91433032060f9a2d43ba
david bartz <the2baddavid@gmail.com>
2012-04-24 23:25:48 -0400

    Function Corrected for uploading banner

--- sql/update/update_banner.php
+++ sql/update/update_banner.php
@@ -1,10 +1,17 @@
 <?php
 
+
+
+/*  Database stuff, not needed right now
 include_once('php/MySQLDatabase/MySQLDatabaseConn.php');
-
 $conn = new MySQLDatabaseConn($GIVE_MYSQL_SERVER, $GIVE_MYSQL_DATABASE, $GIVE_MYSQL_UNAME, $GIVE_MYSQL_PASS);
-$update = array();
+*/
 
+
+/****************************************************************************
+ * Check for Login
+ ****************************************************************************/
+/*
 //to hold various error states
 $error = array();
 
@@ -25,8 +32,17 @@ if(isset($_GET['except'])) {
 if(isset($_GET['code'])) {
 	$error['code'] = $_GET['code'];
 }
+*/
 
-upload_banner($conn, $_FILES);
+/****************************************************************************
+ * GO!
+ ****************************************************************************/
+
+update_banner_simple($_FILES);
+
+/****************************************************************************
+ * FUNCTIONS
+ ****************************************************************************/
 
 /**
  *  Function takes image named in form and copies it into the img folder
@@ -34,40 +50,55 @@ upload_banner($conn, $_FILES);
  * @param type $con     Database connection
  * @param type $files  $_FILES variable
  */
-function upload_banner($con, $files){
-    if ($files['banner']['error'] === UPLOAD_ERR_OK)
-    {
-            $name = "img/".$files['banner']['name'];
-            move_uploaded_file($files['banner']['tmp_name'], $name) or die("bad move");
-            echo "Uploaded image '$name'<br />";
-            update_banner($con, $files['banner']['name']);
-    }
-
-    else {
-        echo "<p> upload error! lol ".$files['banner']['error']."</p>";
-    }
-}
-
-/**
- *  Updates GIVE Center Website Banner
- * @param type $con Database connection
- * @param type $path Path to New 
- */
-function update_banner($con,$path)
-{
-    $query = "UPDATE image_paths
-                SET path = $path
-                WHERE name = 'banner'";
-    $con->query($query);
-}
-
+function update_banner_simple($files){
 /*
- <form action="sql/update/update_banner.php" method="post" enctype="multipart/form-data" name="uploadImage" id="uploadImage">
-        <input type="file" name="banner" id="banner" />
-        <input type="submit" value="Send"></div>
-    </form> 
- 
+ *  update_banner.php
+ * 
+ * Simple update just copies the banner in and overwrites the old one so 
+ * no database work is needed
+ * 
+ * Make sure the file is sent to here, and that it labels it banner
+ * 
+ *  DONT FORGET TO ADD PERMISSIONS FOR www-data ON THE TARGET FOLDER
+ * 
+ *  Sample Form:
+ * 
+ * <html><head><title>PHP Form Upload</title></head><body>
+    <form method='post' action='update_banner.php' enctype='multipart/form-data'>
+        Select File: <input type='file' name='banner' size='10' />
+        <input type='submit' value='Upload' />
+    </form>
  */
+    
+echo <<<_END
+    <html><head><title>PHP Form Upload</title></head><body>
+_END;
 
+
+if(count($files)){
+    if($files['banner']['type']!= 'image/jpeg') 
+        die ("<p>File must be a jpg<br/>
+                ".$files['banner']['type']."</p></body></html>");
+    
+    $path = '../../img/give_banner.jpg';
+    echo '<p>'.$path.'</p>';
+    
+    echo "<pre>";
+        print_r ($files);
+    echo "</pre>";
+    
+    
+    if(!copy($files['banner']['tmp_name'], $path) ){ 
+        echo "error ".$files['banner']['error'];
+    }
+    else{
+        echo "<img src=$path id='banner' ></img>";
+    }
+}   
+
+echo "</body></html>";
+
+header('../Admin.php');
+}
 
 ?>
