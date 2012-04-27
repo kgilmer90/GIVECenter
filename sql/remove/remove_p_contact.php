5d6d1074236ca1186c6a4abcf5d52e1725c20e28
david bartz <the2baddavid@gmail.com>
2012-04-27 01:12:45 -0400

    changed query to use fetch

--- sql/remove/remove_p_contact.php
+++ sql/remove/remove_p_contact.php
@@ -7,12 +7,14 @@
  * remove p contact and set null in program/agency
  */
 
-function remove_p_contact($conn,$program_id){
+function remove_p_contact_program($conn,$program_id){
     
     $query1 = "SELECT p_contact
         FROM program
         WHERE id = $program_id";
-    $contact_id = $conn->query($query1);
+    $conn->query($query1);
+    $contact_id = $conn->fetchRowAsAssoc();
+    $contact_id = $contact_id['p_contact'];
     
     $query2 = "DELETE FROM pro_contact
         WHERE id = $contact_id";
@@ -24,26 +26,22 @@ function remove_p_contact($conn,$program_id){
     $conn->query($query3);
 }
 
-/*CREATE TABLE pro_contact(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
-    title VARCHAR(30),
-    l_name VARCHAR(30),
-    f_name VARCHAR(30),
-    m_name VARCHAR(30),
-    suf VARCHAR(3),
-    m_phone CHAR(10),
-    w_phone CHAR(10),
-    mail VARCHAR(40)) ENGINE INNODB;
- * CREATE TABLE program(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    agency INT UNSIGNED NOT NULL,
-    addr INT,
-    name VARCHAR(20),
-    p_contact INT UNSIGNED NOT NULL,
-    s_contact INT UNSIGNED NOT NULL,
-    descript VARCHAR(400),
-    referal BOOL,
-    notes VARCHAR(400),
-    duration VARCHAR(50)) ENGINE INNODB;
- */
+function remove_p_contact_agency($conn,$agency_id){
+    
+    $query1 = "SELECT p_contact
+        FROM agency
+        WHERE id = $agency_id";
+    $conn->query($query1);
+    $contact_id = $conn->fetchRowAsAssoc();
+    $contact_id = $contact_id['p_contact'];
+    
+    $query2 = "DELETE FROM pro_contact
+        WHERE id = $contact_id";
+    $conn->query($query2);
+    
+    $query3 = "UPDATE agency
+        SET p_contact = 'null'
+        WHERE id = $agency_id";
+    $conn->query($query3);
+}
 ?>
