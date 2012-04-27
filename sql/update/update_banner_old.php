a715ffbcb81c2714ba4b428de5fa6715dea07d34
david bartz <the2baddavid@gmail.com>
2012-04-26 22:57:17 -0400

    Made Eligible to be its own file so it simply needs to be directed to

--- sql/update/update_banner_old.php
+++ sql/update/update_banner_old.php
@@ -1,5 +1,47 @@
 <?php
 
+include_once('../../php/MySQLDatabase/MySQLDatabaseConn.php');
+$conn =0;
+try{
+$conn = new MySQLDatabaseConn($GIVE_MYSQL_SERVER, $GIVE_MYSQL_DATABASE, $GIVE_MYSQL_UNAME, $GIVE_MYSQL_PASS);
+}
+catch(Exception $e){
+    echo $e;
+}
+
+/****************************************************************************
+ * Check for Login
+ ****************************************************************************/
+
+//to hold various error states
+$error = array();
+
+//set to 'failed' if previous login attempt yielded no matches for the uname/pass combo
+if(isset($_GET['login'])) {
+	$error['login'] = $_GET['login'];
+}
+//set to 'true' if user was redirected to the login page via another page's logout button
+if(isset($_GET['logout'])) {
+	$error['logout'] = $_GET['logout'];
+}
+//set to 'conn' if an exception was thrown during login connecting to the database
+//set to 'query' if an exception was thrown during login querying the database
+if(isset($_GET['except'])) {
+	$error['except'] = $_GET['except'];
+}
+//set to the error code resulting from an exception during login
+if(isset($_GET['code'])) {
+	$error['code'] = $_GET['code'];
+}
+
+
+/*********************************************8******************************
+ * Run Update
+ * **************************************************************************/
+
+update_banner_old($conn, $_POST);
+header("../../add_edit.php");
+
 /**
  *
  * @param type $conn 
