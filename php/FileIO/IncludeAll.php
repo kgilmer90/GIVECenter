c6f36100cf31c1f15720bbce119ddbd1ce5a32ed
Ian Stewart <ianstewart30223@gmail.com>
2012-04-29 21:01:57 -0400

    Added CSS definitions to limit the size of dropdown boxes on Admin page

--- php/FileIO/IncludeAll.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once(dirname(__FILE__).'/AbstractFile.php'); 
-include_once(dirname(__FILE__).'/IOException.php');
-include_once(dirname(__FILE__).'/FileFoundException.php');
-include_once(dirname(__FILE__).'/FileNotFoundException.php');
-include_once(dirname(__FILE__).'/FileReader.php');
-include_once(dirname(__FILE__).'/FileWriter.php');
-?>
